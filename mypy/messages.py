a5f39df93a91b4968bfb523ea59f1b8b78304492
KotlinIsland <kotlinisland@users.noreply.github.com>
2022-02-24 21:58:28 +1000

    baseline 2
    - store targets
    - baseline format
    - tests
    - offset
    - refactor
    - handle reveals
    - column number
    - 1 to 1 matching
    - auto baseline write (when no new errors) with flag
    - don't consider reveals for baseline
    - when --write-baseline only show new errors
    - unlink baseline
    - store error target in baseline
    - better errors
    - better messages
    - filter attached notes from errors

--- mypy/messages.py
+++ mypy/messages.py
@@ -210,6 +210,7 @@ class MessageBuilder:
         origin: Context | None = None,
         offset: int = 0,
         allow_dups: bool = False,
+        notes: list[str] | None = None,
     ) -> None:
         """Report an error or note (unless disabled).
 
@@ -250,6 +251,7 @@ class MessageBuilder:
             end_column=context.end_column if context else -1,
             code=code,
             allow_dups=allow_dups,
+            notes=notes,
         )
 
     def fail(
@@ -260,9 +262,12 @@ class MessageBuilder:
         code: ErrorCode | None = None,
         file: str | None = None,
         allow_dups: bool = False,
+        notes: list[str] | None = None,
     ) -> None:
         """Report an error message (unless disabled)."""
-        self.report(msg, context, "error", code=code, file=file, allow_dups=allow_dups)
+        self.report(
+            msg, context, "error", code=code, file=file, allow_dups=allow_dups, notes=notes
+        )
 
     def note(
         self,
